6aecbb8a3277062493dc9a80d52ffe80dadbcd1b
JesdanRS <imajesus08@gmail.com>
2025-05-26 08:48:47 -0400

    Boton de borrar y de aceptacion en todo agregado

--- src/main/java/com/example/atdd/model/Todo.java
+++ src/main/java/com/example/atdd/model/Todo.java
@@ -1,14 +1,23 @@
 package com.example.atdd.model;
 
+import java.util.UUID;
+
 public class Todo {
+    private String id;
     private String text;
 
-    public Todo() {}
+    public Todo() {
+        this.id = UUID.randomUUID().toString();
+    }
 
     public Todo(String text) {
+        this.id = UUID.randomUUID().toString();
         this.text = text;
     }
 
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    
     public String getText() { return text; }
     public void setText(String text) { this.text = text; }
 }
